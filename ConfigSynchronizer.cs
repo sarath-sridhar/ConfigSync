d93f593f63fc079f1b63a9843713eee665e9474c
sarath-sridhar <sarath.pcet@gmail.com>
2025-02-21 22:59:26 -0600

    Created Synchronizer class, Added Dependencies, Built basic core skeleton to read config files

---
+++ ConfigSynchronizer.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace ConfigSync
+{
+    public class ConfigSynchronizer
+    {
+        private readonly IConfiguration _sourceConfig;
+        private IConfiguration _targetConfig;
+
+        public ConfigSynchronizer(IConfiguration sourceConfig)
+        {
+            _sourceConfig = sourceConfig; // The "main" settings to sync from
+        }
+
+        public ConfigSynchronizer FromJson(string jsonPath)
+        {
+            _targetConfig = new ConfigurationBuilder()
+                .AddJsonFile(jsonPath)
+                .Build();
+            return this; 
+        }
+
+        public ConfigSynchronizer ToEnvVars()
+        {
+            _targetConfig = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+            return this;
+        }
+
+        public void Sync()
+        {
+            //ToDo: sync logic
+        }
+
+        public Dictionary<string, string> GetDifferences()
+        {
+            var diffs = new Dictionary<string, string>();
+            foreach (var pair in _sourceConfig.AsEnumerable())
+            {
+                string targetValue = _targetConfig[pair.Key];
+                if (targetValue != pair.Value && pair.Value != null)
+                {
+                    diffs[pair.Key] = $"From {targetValue} to {pair.Value}";
+                }
+            }
+            return diffs;
+        }
+    }
+}
